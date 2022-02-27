f0db4a8202cf24a4caba8a35a76b6b6cceac37c0
Salus2224 <35985669+Salus2224@users.noreply.github.com>
2022-02-27 14:55:04 -0800

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -2203,7 +2203,6 @@ function Library:CreateWindow(WindowTitle)
                     while task.wait() do -- garbage fix for something i don't want to read into (tab doesnt properly color when selected default
                         if TabButton.BackgroundColor3 == Library.BackgroundColor then
                             TabButton.BackgroundColor3 = Library.MainColor;
-                            warn("fixed color");
                             break;
                         end;
                     end;
