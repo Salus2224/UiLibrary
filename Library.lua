07a9acba1ef631409b1eba9f8eb02cd1cd79078e
Salus2224 <35985669+Salus2224@users.noreply.github.com>
2022-03-30 22:09:42 -0700

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -898,7 +898,7 @@ do
         });
     end;
 
-    function Funcs:AddLabel(Text, Color)
+    function Funcs:AddLabel(Text, Color, Wrapped)
         local Label = {};
 
         local Groupbox = self;
@@ -908,6 +908,7 @@ do
             Size = UDim2.new(1, -4, 0, 15);
             TextSize = 14;
             Text = Text;
+            TextWrapped = Wrapped;
             TextXAlignment = Enum.TextXAlignment.Left;
             ZIndex = 5;
             Parent = Container;
